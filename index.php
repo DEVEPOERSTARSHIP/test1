0d5b7972f44a445b822c8af0a56e3d35ad8e075d
DEVEPOERSTARSHIP <desarrollo@mistarship.com>
2024-11-11 15:22:54 -0600

    subiendo a rama1

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GIT </title>
+</head>
+<body>
+    <p>inicio de tema de git</p>
+    <p>subiendo a la rama</p>
+</body>
+</html>
